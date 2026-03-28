e319a0656c2037d1d132c50696b9856b3f3fae94
MarcHunter87 <marchunter888@gmail.com>
2026-03-28 11:47:58 +0100

    Daily Xuxemons Works

--- xuxemons-backend/app/Http/Controllers/AdminController.php
+++ xuxemons-backend/app/Http/Controllers/AdminController.php
@@ -17,6 +17,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 use Throwable;
--- xuxemons-backend/routes/console.php
+++ xuxemons-backend/routes/console.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Models\DailyReward;
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    $dailyXuxemonTime = DailyReward::where('reward_type', 'daily_xuxemon')
+        ->value('time');
+
+    if (! $dailyXuxemonTime) {
+        return;
+    }
+
+    $timeFormatted = Carbon::parse($dailyXuxemonTime)->format('H:i');
+
+    if (Carbon::now()->format('H:i') === $timeFormatted) {
+        Artisan::call('app:process-daily-xuxemons');
+        echo Artisan::output();
+    }
+})->everyMinute();
